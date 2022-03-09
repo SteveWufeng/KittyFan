31f3e5c75f29cd837cdd3cb67f2f74c586089aad
SteveWu <86623538+SteveWufeng@users.noreply.github.com>
2022-03-09 18:31:38 -0500

    renamed

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# rpi_ws281x library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
-
 import time
 from rpi_ws281x import *
 import argparse
